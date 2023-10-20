8e77e6345c2688afc56c48ed59b84ebaae344430
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-10-20 13:13:25 +0200

    Add project files.

---
+++ Inlämningsuppgift 4.5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace uppgift_4._5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string heltal = Console.ReadLine();
+
+            for (int i = 0;i<heltal.Length;i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                int x = siffra+1;
+                if (x == 10)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.Write(x);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
